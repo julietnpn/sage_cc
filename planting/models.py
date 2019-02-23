c1a0bc903f004ab237513e955bbfc7a9d48ed06d
julietnpn <jnnorton@uci.edu>
2019-02-23 13:59:02 -0800

    Created User and Yard models, Mirgrated Models, Put data in the database (Human use still needs to be completed)

--- planting/models.py
+++ planting/models.py
@@ -2,75 +2,106 @@ from django.db import models
 
 # Create your models here.
 
-#-----Plant Model-----#
-class Plant(models.Model):
+#-----User Model-----#
+class User(models.Model):
 	name = models.CharField(max_length=300, blank=True, null=True)
-	layer = models.ForeignKey('Layer', on_delete=models.CASCADE, blank=True, null=True)
-	plantImage = CharField(max_length=500, blank=True, null=True)
-
+	
 	class Meta:
 		managed = True
-		db_table = 'plant'
+		db_table = 'user'
+		
+	def __str__(self):
+		return self.name
+
+#-----Yards Model-----#
+class Yards(models.Model):
+	plant_id = models.ForeignKey('Plant', on_delete=models.CASCADE, blank=True, null=True)
+	user_id = models.ForeignKey('User', on_delete=models.CASCADE, blank=True, null=True)
+	
+	class Meta:
+		managed = True
+		db_table = 'yard'
 
 
 #-----Layer Model-----#
 class Layer(models.Model):
-    value = models.CharField(max_length=300, blank=True, null=True)
+	value = models.CharField(max_length=300, blank=True, null=True)
 
-    class Meta:
-        managed = True
-        db_table = 'layer'
-
-    def __str__(self):
-        return self.value
-
-
-#-----Plant Human Uses---#
-class PlantHumanUses (models.Model):
-        plant_id = models.ForeignKey('Plant', on_delete=models.CASCADE, blank=True, null=True)
-        human_use_property_id = models.ForeignKey('HumanUseProperty', on_delete=models.CASCADE, blank=True, null=True)
-
-         class Meta:
+	class Meta:
 		managed = True
-		db_table = 'plantHumanUses'               
+		db_table = 'layer'
+
+	def __str__(self):
+		return self.value
+
+
+#-----EcosystemRelationshipProperty (ERP)-----#
+class EcosystemRelationshipProperty(models.Model):
+	attribute = models.CharField(max_length = 300)
+
+	class Meta:
+		managed = True
+		db_table = 'ecosystemRelationshipProperty'
+
+	def __str__(self):
+		return self.attribute
+
+
+#-----EcosystemRelationshipValue (ERV)-----#
+class EcosystemRelationshipValue(models.Model):
+	value = models.CharField(max_length=50, blank=True, null=True)
+
+	class Meta:
+		managed = True
+		db_table = "ecosystemRelationshipValue"
+		
+	def __str__(self):
+		return self.value
+
+
+#-----HumanUseProperty Model-----#
+class HumanUseProperty(models.Model):
+	attribute = models.CharField(max_length=300, blank=True, null=True)
+
+	class Meta:
+		managed = True
+		db_table = 'humanUseProperty'
+	
+	def __str__(self):
+		return self.attribute
+
+
 
 
 #-----PlantEcosystemRelationship Model-----#
 class PlantEcosystemRelationship(models.Model):
 	plant_id = models.ForeignKey('Plant', on_delete=models.CASCADE, blank=True, null=True)
 	ecosystemRelationshipProperty = models.ForeignKey('EcosystemRelationshipProperty', on_delete=models.CASCADE, blank=True, null=True)
-	ecosystemRelationshipValue = models.ForeignKey('EcosystemRelationshipValues', on_delete=models.CASCADE, blank=True, null=True)
+	ecosystemRelationshipValue = models.ForeignKey('EcosystemRelationshipValue', on_delete=models.CASCADE, blank=True, null=True)
 
 	class Meta:
 		managed = True
 		db_table = 'plantEcosystemRelationship'
 
 
-#-----HumanUseProperty Model-----#
-class HumanUseProperty(models.Model):
-    property = models.CharField(max_length=300, blank=True, null=True)
 
-    class Meta:
-        managed = True
-        db_table = 'humanUseProperty'
+#-----Plant Human Use---#
+class PlantHumanUse (models.Model):
+	plant_id = models.ForeignKey('Plant', on_delete=models.CASCADE, blank=True, null=True)
+	human_use_property_id = models.ForeignKey('HumanUseProperty', on_delete=models.CASCADE, blank=True, null=True)
 
-
-#-----EcosystemRelationshipProperty (ERP)-----#
-class EcosystemRelationshipProperty(models.Model):
-	plantProperty = models.CharField(max_length = 300)
-
-	class Meta: 
+	class Meta:
 		managed = True
-		db_table = 'EcosystemRelationshipProperty'
-
-	def __str__(self):
-	    return self.value
+		db_table = "plantHumanUse"
 
 
-#-----EcosystemRelationshipValue (ERV)-----#
-class EcosystemRelationshipValue(models.Model):
-    value = models.CharField(max_length=50, blank=True, null=True)
 
-    class Meta:
-        managed = True
-        db_table = "EcosystemRelationshipValue"
+#-----Plant Model-----#
+class Plant(models.Model):
+	name = models.CharField(max_length=300, blank=True, null=True)
+	layer = models.ForeignKey('Layer', on_delete=models.CASCADE, blank=True, null=True)
+	imgURL = models.URLField(blank=True, null=True)
+
+	class Meta:
+		managed = True
+		db_table = 'plant'
