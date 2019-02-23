2d61a9e0098d4c7cc7721ec77db24a3489fe6c77
julietnpn <jnnorton@uci.edu>
2019-02-23 09:04:49 -0800

    created the plants and layer model for example

--- planting/models.py
+++ planting/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+#---Plant Model-----#
+class Plant(models.Model):
+	name = models.CharField(max_length=300, blank=True, null=True)
+	layer = models.ForeignKey('Layer', on_delete=models.CASCADE, blank=True, null=True)
+
+	class Meta:
+		managed = True
+		db_table = 'plant'
+
+class Layer(models.Model):
+    value = models.TextField(blank=True, null=True)
+
+    class Meta:
+        managed = True
+        db_table = 'layer'
+
+    def __str__(self):
+        return self.value
