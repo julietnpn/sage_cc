ac4659db47d37e35cbfaa71383f7f90353ff800f
Birgit Penzenstadler <birgit@penzenstadler.de>
2019-02-23 10:45:10 -0800

    added models for plant ecosystem relatinoship and human use property

--- planting/models.py
+++ planting/models.py
@@ -12,7 +12,7 @@ class Plant(models.Model):
 		db_table = 'plant'
 
 class Layer(models.Model):
-    value = models.TextField(blank=True, null=True)
+    value = models.CharField(max_length=300, blank=True, null=True)
 
     class Meta:
         managed = True
@@ -20,3 +20,22 @@ class Layer(models.Model):
 
     def __str__(self):
         return self.value
+
+
+#---PlantEcosystemRelationship Model-----#
+class PlantEcosystemRelationship(models.Model):
+	plant_id = models.ForeignKey('Plant', on_delete=models.CASCADE, blank=True, null=True)
+	ecosystemRelationshipProperty = models.ForeignKey('EcosystemRelationshipProperty', on_delete=models.CASCADE, blank=True, null=True)
+	ecosystemRelationshipValue = models.ForeignKey('EcosystemRelationshipValues', on_delete=models.CASCADE, blank=True, null=True)
+
+	class Meta:
+		managed = True
+		db_table = 'plantEcosystemRelationship'
+
+
+class HumanUseProperty(models.Model):
+    property = models.CharField(max_length=300, blank=True, null=True)
+
+    class Meta:
+        managed = True
+        db_table = 'humanUseProperty'
