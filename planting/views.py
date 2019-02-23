22c11f239ccf4325cb08e0ae0c42085d51a3b200
Jainee Shah <jaineeshah16@gmail.com>
2019-02-23 15:19:04 -0800

    updated version

--- planting/views.py
+++ planting/views.py
@@ -2,4 +2,8 @@ from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the planting index.")
+    alex = Users.objects.get(name = 'Alex')
+    blake = Users.objects.get(name = 'Blake')
+    alex_yard = Yards.objects.get(user_id = alex.id)
+    blake_yard = Yards.objects.get(user_id = blake.id)
+    #return HttpResponse("Hello, world. You're at the planting index.")
