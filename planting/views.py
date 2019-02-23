1e5f48878e016b6c4c8aea1f5e8b2eb17d605c35
Emily Kang <emily.kang98@gmail.com>
2019-02-23 15:58:04 -0800

    Updated views

--- planting/views.py
+++ planting/views.py
@@ -2,8 +2,24 @@ from django.http import HttpResponse
 
 
 def index(request):
-    alex = Users.objects.get(name = 'Alex')
-    blake = Users.objects.get(name = 'Blake')
+    alex = User.objects.get(name = 'Alex')
+    blake = User.objects.get(name = 'Blake')
     alex_yard = Yards.objects.get(user_id = alex.id)
     blake_yard = Yards.objects.get(user_id = blake.id)
+
+    alex_plants = []
+    alex_plant_urls = []
+
+    for y in alex_yard:
+        alex_plants.append(Plant.objects.get(id = alex_yard.plant))
+
+    for x in alex_plants:
+        plant_urls.append(Plant.objects.get(imgURL))
+
+
+    context = {
+        'alexPlantUrls': alex_plant_urls
+    }    
+
+                        
     #return HttpResponse("Hello, world. You're at the planting index.")
