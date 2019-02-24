af75f29568bd7d2d03bd57e33cb952497c3dedf9
Sam M <SamMcDonald@dhcp-39-41-116.dhcpint.csulb.edu>
2019-02-23 16:00:23 -0800

    add loop for urls

--- planting/views.py
+++ planting/views.py
@@ -7,3 +7,20 @@ def index(request):
     alex_yard = Yards.objects.get(user_id = alex.id)
     blake_yard = Yards.objects.get(user_id = blake.id)
     #return HttpResponse("Hello, world. You're at the planting index.")
+
+alex_plants = []
+alex_plant_urls = []
+
+for y in alex_yard:
+    alex_plants.append(Plant.objects.get(id = alex_yard.plant))
+
+for x in alex_plants:
+    plant_urls.append(Plant.objects.get(imgURL))
+
+
+context = {
+    'alexPlantUrls': alex_plant_urls
+}    
+
+                    
+#return HttpResponse("Hello, world. You're at the planting index.")
