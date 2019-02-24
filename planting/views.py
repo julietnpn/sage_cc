96a7c41d13fc49fceb0f541f12d9b81ff9711d45
julietnpn <jnnorton@uci.edu>
2019-02-23 22:32:32 -0800

    pseudocode for emily to get second page up-and-running!!!

--- planting/views.py
+++ planting/views.py
@@ -10,30 +10,54 @@ def index(request):
     blake_yard = Yards.objects.filter(user_id = blake.id)
 
     alex_plants = []
-    alex_plant_urls = []
+    #alex_plant_urls = []
 
     for y in alex_yard:
         alex_plants.append(Plant.objects.get(id = y.plant.id))
 
-    for x in alex_plants:
-        alex_plant_urls.append(x.imgURL)
+    #for x in alex_plants:
+        #alex_plant_urls.append(x.imgURL)
         
     blake_plants = []
-    blake_plant_urls = []
+    #blake_plant_urls = []
 
     for y in blake_yard:
         blake_plants.append(Plant.objects.get(id = y.plant.id))
 
-    for x in blake_plants:
-        blake_plant_urls.append(x.imgURL)
+    #for x in blake_plants:
+        #blake_plant_urls.append(x.imgURL)
 
 
     context = {
-        'alexPlantUrls': alex_plant_urls,
-        'blakePlantUrls': blake_plant_urls
+        'alexPlants': alex_plants,
+        'blakePlants': blake_plants
+        #'alexPlantUrls': alex_plant_urls,
+        #'blakePlantUrls': blake_plant_urls
     }
 
     return render(request, 'planting/index.html', context)
 
+
 def suggestions(request):
-    return HttpResponse("Hello, world. Here are suggestions.")
+    #get list of all plantecosystem relationships that are deter or provide
+    #for each of blake plants
+        #get list of plantecosystemrelationships that are needs
+            #for each n in list of blake-needs-plantecosystemrelationship
+                #filter plantecosystems for provides of n platecosystemrelationships property
+                    #add plant and relationship to context
+        #get list of plantecosystemrelationships that are susceptible
+            #for each n in list of blake-susceptible-plantecosystemrelationship
+                #filter plantecosystems for deters of n platecosystemrelationships property
+                    #add plant and relationship to context
+                
+    
+    #Blake's <plant.name> <needs/is susceptible to> <shared property>. You can plant <suggestionplant.name> because it <provides/deters> <shared property> 
+#     context = {
+#         'relationship':
+#             'blake_plant_name':  ,
+#             'blake_plant_need': ,
+#             'shared_plant_property': ,
+#             'suggestion_plant_name':
+#             'suggestion_plant_provides':
+#     }
+    return render(request, 'planting/index.html', context)
